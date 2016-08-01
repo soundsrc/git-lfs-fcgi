39316daf9d8c67afca9d4b1639b469ad22a44ce4
Sound <sound@sagaforce.com>
2016-08-01 10:40:52 -0700

    Adding options struct.

--- standalone_main.c
+++ standalone_main.c
@@ -18,10 +18,13 @@
 #include <string.h>
 #include <ctype.h>
 #include <getopt.h>
+#include "compat/string.h"
+#include "options.h"
 #include "socket_io.h"
 #include "mongoose.h"
 
 static struct mg_context *context = NULL;
+static struct options options;
 static int running = 1;
 
 static void intHandler(int sig)
@@ -62,15 +65,23 @@ static int handleRequest(struct mg_connection *conn)
 int main(int argc, char *argv[])
 {
 	struct mg_callbacks callbacks;
-	const char *options[] = {
+	const char *mg_options[] = {
 		"document_root", ".",
 		"listening_ports", "8080",
 		NULL
 	};
 
+	memset(&options, 0, sizeof(options));
+	if(strlcpy(options.object_path, ".", sizeof(options.object_path)) >= sizeof(options.object_path))
+	{
+		fprintf(stderr, "Object path is too long.\n");
+		return -1;
+	}
+	
+	
 	memset(&callbacks,0,sizeof(callbacks));
 	callbacks.begin_request = handleRequest;
-	context = mg_start(&callbacks, NULL, options);
+	context = mg_start(&callbacks, NULL, mg_options);
 	if(!context) {
 		fprintf(stderr,"Failed to start server.\n");
 		return -1;
