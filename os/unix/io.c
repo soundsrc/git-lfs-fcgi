2428e6f4332715162681a6c204b011ad4dd50744
Sound <sound@sagaforce.com>
2017-07-24 20:25:46 -0700

    Replace premake with cmake.

--- os/unix/io.c
+++ os/unix/io.c
@@ -13,7 +13,7 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-#include "io.h"
+#include "os/io.h"
 #include <unistd.h>
 #include <fcntl.h>
 
