c6fe05eebe30f799851682a4e9e5ced62b06c557
Sound <sound@sagaforce.com>
2017-07-26 21:44:13 -0700

    Compile fixes for linux.

--- external/libfcgi/include/fcgi_config.h
+++ external/libfcgi/include/fcgi_config.h
@@ -38,12 +38,41 @@
 #undef inline
 #undef ssize_t
 
-#ifndef _MSC_VER
-#define STDC_HEADERS 1
-#define HAVE_FILENO_PROTO 1
-#define HAVE_NETINET_IN_H 1
-#define HAVE_NETDB_H 1
-#define HAVE_SYS_SOCKET_H 1
-#define HAVE_UNISTD_H 1
-#define HAVE_SOCKADDR_UN_SUN_LEN 1
+#ifdef __linux__
+#	define HAVE_ARPA_INET_H 1
+#	define HAVE_DLFCN_H 1
+#	define HAVE_FILENO_PROTO 1
+//	#define HAVE_FPOS 1
+#	define HAVE_INTTYPES_H 1
+#	define HAVE_LIBNSL 1
+//	#define HAVE_LIMITS_H 1
+#	define HAVE_MEMORY_H 1
+#	define HAVE_NETDB_H 1
+#	define HAVE_NETINET_IN_H 1
+#	define HAVE_STDINT_H 1
+#	define HAVE_STDLIB_H 1
+//	#define HAVE_STREAMBUF_CHAR_TYPE 1
+//	#define HAVE_STRERROR 1
+#	define HAVE_STRINGS_H 1
+#	define HAVE_STRING_H 1
+#	define HAVE_SYS_PARAM_H 1
+#	define HAVE_SYS_SOCKET_H 1
+#	define HAVE_SYS_STAT_H 1
+#	define HAVE_SYS_TIME_H 1
+#	define HAVE_SYS_TYPES_H 1
+#	define HAVE_UNISTD_H 1
+#	define PACKAGE "fcgi"
+#	define STDC_HEADERS 1
+#	define VERSION "2.4.0"
+#else
+#	ifndef _MSC_VER
+#		define STDC_HEADERS 1
+#		define HAVE_FILENO_PROTO 1
+#		define HAVE_NETINET_IN_H 1
+#		define HAVE_NETDB_H 1
+#		define HAVE_SYS_SOCKET_H 1
+#		define HAVE_UNISTD_H 1
+#		define HAVE_SOCKADDR_UN_SUN_LEN 1
+#	endif
 #endif
+
