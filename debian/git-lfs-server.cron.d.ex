8b06b8e41280d02b6a935ba91d1fb1ffaf24b3e2
Sound <sound@sagaforce.com>
2018-01-20 13:32:04 -0800

    Remove debian example files.

--- debian/git-lfs-server.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the git-lfs-server package
-#
-0 4	* * *	root	[ -x /usr/bin/git-lfs-server_maintenance ] && /usr/bin/git-lfs-server_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(git-lfs-server):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="git-lfs-server" command="/usr/bin/git-lfs-server"
